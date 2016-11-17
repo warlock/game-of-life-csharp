cf0848892881bf4f70386a944fb45bbe962ed9d0
Josep Subils <josep.sr@coditramuntana.com>
2016-11-17 15:29:17 +0100

    First commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLife;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 10;
+            Game.Hi();
+            Game.MakeRandomGrid(x, y);
+            for (var ix = 0; ix < x; ix++)
+            {
+                for (var iy = 0; iy < y; iy++)
+                {
+                    Console.Write(Game.Grid[ix, iy]?"X":"O");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameOfLife/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Game
+    {
+        public static void Hi()
+        {
+            Console.WriteLine("Grid");
+        }
+
+        public static bool[,] Grid { get; set; }
+
+        public static void MakeRandomGrid(int x = 10, int y = 10)
+        {
+            bool[,] ngr = new bool[x,y];
+            var rand = new Random();
+            for (int ix = 0; ix < x; ix++)
+            {
+                for (int iy = 0; iy < y; iy++)
+                {
+                    bool a = rand.Next(0, 2) == 0;
+                    ngr[ix, iy] = a;
+                }
+            }
+            Grid = ngr;
+        }
+
+        private static bool MakeRandomValue()
+        {
+            return new Random().Next(0, 2) == 0;
+        }
+    }
+}
